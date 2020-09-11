39efdc68790e03ba87212c12862f731aa8d47b91
Christina Howard <clhoward33@icloud.com>
2020-09-11 23:52:01 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+phrase = "Hello there, how are you?"
+
+def reverse_each_word(phrase)
+  sentence = []
+  sentence << {"Hello there, how are you?"}
+  phrase.each.reverse! do |phrase|
+
+  end
+
+end
