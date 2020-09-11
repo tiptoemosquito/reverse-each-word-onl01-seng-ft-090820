aebaa3fc6f9564c7cd0002d4536c995f4e09c70d
Christina Howard <clhoward33@icloud.com>
2020-09-11 23:52:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 phrase = "Hello there, how are you?"
 
 def reverse_each_word(phrase)
-  sentence = []
-  sentence << {"Hello there, how are you?"}
+  phrase = []
+  phrase << {"Hello there, how are you?"}
   phrase.each.reverse! do |phrase|
 
   end
