a9f02963fef3bcf938e6ac6a95fffc621736e661
Christina Howard <clhoward33@icloud.com>
2020-09-12 01:08:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,6 @@ def reverse_each_word(word)
   sentence.split.collect {|word| sentence.reverse}.join(" ")
 end
 
-def reverse_each_word(word)
-  word.reverse.split.reverse.join(" ")
-end
+#def reverse_each_word(word)
+  #word.reverse.split.reverse.join(" ")
+#end
