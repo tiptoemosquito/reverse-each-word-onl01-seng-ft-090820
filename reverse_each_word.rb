8644e1b8404f7b3ea084fc1fb72cf6ac90261f1c
Christina Howard <clhoward33@icloud.com>
2020-09-12 00:31:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 word = "Hello, there how are you?"
 def reverse_each_word
-  word.reverse.collect.reverse.join("")
+  word.split.collect.reverse.join ("")
 end
 #def reverse_each_word(word)
   #word.reverse.split.reverse.join(" ")
