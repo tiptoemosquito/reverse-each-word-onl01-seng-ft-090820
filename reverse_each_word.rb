f050e80f42a474dd543a3d1e9cb595ce48e7aa1e
Christina Howard <clhoward33@icloud.com>
2020-09-12 00:47:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
-word = "Hello, there how are you?"
+phrase = "Hello, there how are you?"
 def reverse_each_word
-  word.split.collect {|word| word.reverse}.join(" ")
+  phrase.split.collect {|word| word.reverse}.join(" ")
 end
 #def reverse_each_word(word)
   #word.reverse.split.reverse.join(" ")
